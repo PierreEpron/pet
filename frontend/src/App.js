462fa294b09120c04cf4d91bc3a466663d6dd58d
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-05-05 08:15:41 +0200

    add Header to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div className="App">
+      <Header/>
+    </div>
   );
 }
 
