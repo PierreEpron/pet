37d2257f81d9dc34a226a60ce6622da955154693
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-05-05 08:26:24 +0200

    add const useStyles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,33 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar,Typography, makeStyles } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles(() => ({
+  header: {
+    backgroundColor: "#400CCC",
+  },
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    textAlign: "left",
+  },
+}));
+
 export default function Header() {
+  const { header, logo } = useStyles();
   const displayDesktop = () => {
-    return <Toolbar>Hi From Desktop Header</Toolbar>;
+    return <Toolbar>{PetApp_Nancyclotep}</Toolbar>;
   };
+
+  const PetApp_Nancyclotep = (
+    <Typography variant="h6" component="h1" className={logo}>
+      PetApp_Nancyclotep
+    </Typography>
+  );
 
   return (
     <header>
-      <AppBar>{displayDesktop()}</AppBar>
+      <AppBar className={header}>{displayDesktop()}</AppBar>
     </header>
   );
 }
