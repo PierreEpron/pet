9b95d55175438c9516f3261e7e603be469f43073
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-05-05 11:09:13 +0200

    add Signin not work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from "./components/Header";
 import SignIn from "./components/SignIn";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-        <SignIn/>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={() => <SignIn />} />
+          <Route path="/Header" exact component={() => <Header />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
