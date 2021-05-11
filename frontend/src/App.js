a7ad91432b287b09ff926a9e289d38181e4eb596
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-05-11 11:01:02 +0200

    add page 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import SignIn from "./components/SignIn";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Pages1 from "./pages/Pages1"
 import Pages2 from "./pages/Pages2"
+import Pages3 from "./pages/Pages3"
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact component={() => <SignIn />} />
-            <Route path="/pages1" exact component={() => <Pages1 />} />*
+            <Route path="/pages1" exact component={() => <Pages1 />} />
             <Route path="/pages2" exact component={() => <Pages2 />} />
+            <Route path="/pages3" exact component={() => <Pages3 />} />
             {/*<Route path="/pagestest" exact component={() => <PageTest />} />*/}
         </Switch>
       </Router>
