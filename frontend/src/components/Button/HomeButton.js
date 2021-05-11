e4e20c4dfc5b2c34804382ff766904b9011b367a
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-05-11 12:10:18 +0200

    add rout & rederict

---
+++ frontend/src/components/Button/HomeButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+function redirection(e){
+    e.preventDefault()
+    console.log("redirection")
+    window.location = "/pages1"
+  }
+
+
+export default function ContainedButtons() {
+  const classes = useStyles();
+return (
+    <div className={classes.root}>
+      <Button variant="contained" color="primary" onClick={redirection}>
+        Acceuil
+      </Button>
+    </div>
+  );
+}
