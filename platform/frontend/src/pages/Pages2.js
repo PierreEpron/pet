5e5e68d0edb7d61a97b6407f16e9562427202845
micka-sudo <82089192+micka-sudo@users.noreply.github.com>
2021-05-13 09:52:49 +0200

    add container, grid

--- platform/frontend/src/pages/Pages2.js
+++ platform/frontend/src/pages/Pages2.js
@@ -15,6 +15,9 @@ import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Header from "../components/Header";
+import Container from "@material-ui/core/Container";
+import Grid from '@material-ui/core/Grid';
+
 
 const useRowStyles = makeStyles({
   root: {
@@ -23,6 +26,32 @@ const useRowStyles = makeStyles({
     },
   },
 });
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  // appBarSpacer: theme.mixins.toolbar,
+  // content: {
+  //   flexGrow: 1,
+  //   height: '100vh',
+  //   overflow: 'auto',
+  //   paddingRight: 24,
+  // },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
 
 function createData(info, precision, résultat) {
   return {
@@ -110,9 +139,13 @@ const rows = [
 ];
 
 export default function CollapsibleTable() {
+  const classes = useStyles();
   return (
     <div>
     <Header/>
+    <Container maxWidth="lg" className={classes.container}>
+    <Grid container spacing={2}>
+    <Grid xs={12} sm={6}>
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
         <TableHead>
@@ -130,6 +163,9 @@ export default function CollapsibleTable() {
         </TableBody>
       </Table>
     </TableContainer>
+    </Grid>
+    </Grid>
+    </Container>
     </div>
   );
 }
