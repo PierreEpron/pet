d23ea90e3fd0b4e7391e390b453fb8cb9516adf3
PierreEpron <pierre.epron@gmail.com>
2021-05-14 15:59:21 +0200

    add superadmin creation in  backend/start.sh

--- platform/backend/start.sh
+++ platform/backend/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 python3 manage.py migrate
+
+
+echo "Creating admin if not exist"
+python manage.py create_admin \
+--username "${ADMIN_USERNAME}" \
+--password "${ADMIN_PASSWORD}" \
+--email "${ADMIN_EMAIL}" \
+--noinput \
+
+
 python3 manage.py runserver 0.0.0.0:8000
