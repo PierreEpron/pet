2e4d3b15d8ff238ba7b2458a942cab9498a50361
PierreEpron <pierre.epron@gmail.com>
2021-05-19 12:52:43 +0200

    Make ContentSerializer create

--- platform/backend/api/serializers.py
+++ platform/backend/api/serializers.py
@@ -3,15 +3,32 @@ from rest_framework import serializers
 from .models import ExamWording # , ExamRoom , Exam
 
 class ContentSerializer(serializers.ModelSerializer):
+    FIELDS = ['modified_by', 'created_by', 'created_date' , 'modified_date']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+
+        if user.is_anonymous:
+            validated_data['created_by'] = None
+            validated_data['modified_by'] = None
+        else:
+            validated_data['created_by'] = user
+            validated_data['modified_by'] = user
+
+        return super().create(validated_data)
+
     def update(self, instance, validated_data):
         user = self.context['request'].user
-        validated_data['modified_by'] = None if user.is_anonymous else user
+        if user.is_anonymous:
+            validated_data['modified_by'] = None  
+        else:
+            validated_data['modified_by'] = user
         return super().update(instance, validated_data)
 
 class ExamWordingSerializer(ContentSerializer):
     class Meta:
         model = ExamWording
-        fields = ['word', 'modified_by', 'created_by', 'created_date' , 'modified_date']
+        fields = ['word'] + ContentSerializer.FIELDS
 
 
 # class ExamSerializer(serializers.ModelSerializer):
