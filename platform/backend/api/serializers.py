4a7c7f75c65c0e9a175ea5b46dad2c61cf89b29a
PierreEpron <pierre.epron@gmail.com>
2021-05-19 12:31:55 +0200

    Make ContentSerializer update

--- platform/backend/api/serializers.py
+++ platform/backend/api/serializers.py
@@ -2,8 +2,13 @@ from django.db import models
 from rest_framework import serializers
 from .models import ExamWording # , ExamRoom , Exam
 
+class ContentSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        user = self.context['request'].user
+        validated_data['modified_by'] = None if user.is_anonymous else user
+        return super().update(instance, validated_data)
 
-class ExamWordingSerializer(serializers.ModelSerializer):
+class ExamWordingSerializer(ContentSerializer):
     class Meta:
         model = ExamWording
         fields = ['word', 'modified_by', 'created_by', 'created_date' , 'modified_date']
