103792c3fe1d1bb7dde2d5b3765d7648ab526980
PierreEpron <pierre.epron@gmail.com>
2021-05-22 13:11:49 +0200

    setup cors

--- platform/backend/backend/settings.py
+++ platform/backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'authentification.apps.AuthentificationConfig',
     'django_filters',
+    'corsheaders',      
     'server.apps.ServerConfig',
     'api.apps.ApiConfig',    
 ]
@@ -52,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -140,3 +143,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get("FRONTEND_URL"),
+]
