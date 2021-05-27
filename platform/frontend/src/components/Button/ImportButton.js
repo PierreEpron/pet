3b6ac845c685bf7acc4eaba0eb3d7739f2cd2509
MickaSimplon <mickael.simlon@gmail.com>
2021-05-27 15:53:36 +0200

    change import circle progress

--- platform/frontend/src/components/Button/ImportButton.js
+++ platform/frontend/src/components/Button/ImportButton.js
@@ -3,8 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import {upload} from '../../services/upload.service';
+import clsx from 'clsx';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,16 +16,44 @@ const useStyles = makeStyles((theme) => ({
   input: {
     display: 'none',
   },
+    wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+    buttonProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+
 }));
 
-export default function UploadButtons(props) {
+export default function UploadButtons() {
   const classes = useStyles();
-  const { onClick, loading } = props;
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const timer = React.useRef();
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+      timer.current = window.setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 5000);
+    }
+  };
 
   function onFileChange(event) {
-      const file = event.target.files[0];
-      const formData = new FormData();
-
+      const file = event.target.files[0]
+      const formData = new FormData()
       formData.append("csv", file, file.name);
       upload(formData)
   }
@@ -40,11 +69,21 @@ export default function UploadButtons(props) {
         accept=".csv,.xlsx,.xls"
         onChange={onFileChange}
       />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color= 'primary' component="span" onClick={onClick} disabled={loading}>
-      {loading && <CircularProgress size={14} />}
-      {!loading && 'import'}
-    </Button>
+      <label htmlFor="contained-button-file" className={classes.wrapper}>
+        {/*<Button variant="contained" color="primary" component="span">
+          Import
+        </Button>*/}
+        <Button
+          variant="contained"
+          color="primary"
+          className={buttonClassname}
+          disabled={loading}
+          onClick={handleButtonClick}
+          component="span"
+        >
+          Import
+        </Button>
+        {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
       </label>
       <label htmlFor="icon-button-file">
         <IconButton color="primary" aria-label="upload picture" component="span">
