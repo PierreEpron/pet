d62b692106db5cff7a293a50e6f6dbce4aef5c98
MickaSimplon <mickael.simlon@gmail.com>
2021-05-27 09:50:06 +0200

    creat button with logo

---
+++ platform/frontend/src/components/Button/ExitApp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+export default function ExitApp() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="LogOut" icon={<ExitToAppIcon />} />
+    </BottomNavigation>
+  );
+}
