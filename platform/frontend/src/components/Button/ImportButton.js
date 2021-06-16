a705eee922d55b6b736c2e9556c04eb377f1d846
MickaSimplon <mickael.simlon@gmail.com>
2021-06-16 09:44:06 +0200

    test LinearProgress in importbutton

--- platform/frontend/src/components/Button/ImportButton.js
+++ platform/frontend/src/components/Button/ImportButton.js
@@ -5,6 +5,8 @@ import IconButton from '@material-ui/core/IconButton';
 import {upload} from '../../services/upload.service';
 import clsx from 'clsx';
 import CircularProgress from '@material-ui/core/CircularProgress';
+// import LinearProgress from '@material-ui/core/LinearProgress';
+
 import { green } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
@@ -72,6 +74,7 @@ export default function UploadButtons() {
           Import
         </Button>
         {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+          {/*{loading && <LinearProgress color="red" variant="determinate" value={loading}  size={24} className={classes.buttonProgress} />}*/}
       </label>
     </div>
   );
