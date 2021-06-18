f0558b142609f6eb01caf70bb58b1a5921642724
MickaSimplon <mickael.simlon@gmail.com>
2021-06-18 09:59:17 +0200

    rearrange code

--- platform/frontend/src/components/Button/IconButton.js
+++ platform/frontend/src/components/Button/IconButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import ForwardIcon from '@material-ui/icons/Forward';
 
@@ -15,10 +15,10 @@ export default function IconButtons() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <IconButton color="primary" aria-label="go DocPage">
-        <ForwardIcon />
-      </IconButton>
-    </div>
+      <div className={classes.root}>
+        <IconButton color="primary" aria-label="go DocPage">
+          <ForwardIcon/>
+        </IconButton>
+      </div>
   );
 }
