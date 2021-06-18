8ef3a3cb606c390d9355eac01619218a0db67348
MickaSimplon <mickael.simlon@gmail.com>
2021-06-18 09:49:11 +0200

    clean & rearrange code

--- platform/frontend/src/components/Button/ExportButton.js
+++ platform/frontend/src/components/Button/ExportButton.js
@@ -1,42 +1,42 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
     },
-  },
-  input: {
-    display: 'none',
-  },
+    input: {
+        display: 'none',
+    },
 }));
 
 export default function ExportloadButtons() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <input
-        accept=".csv,.xlsx,.xls"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
-          Exporter
-        </Button>
-      </label>
-      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span">
-        </IconButton>
-      </label>
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <input
+                accept=".csv,.xlsx,.xls"
+                className={classes.input}
+                id="contained-button-file"
+                multiple
+                type="file"
+            />
+            <label htmlFor="contained-button-file">
+                <Button variant="contained" color="primary" component="span">
+                    Exporter
+                </Button>
+            </label>
+            <input accept="image/*" className={classes.input} id="icon-button-file" type="file"/>
+            <label htmlFor="icon-button-file">
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                </IconButton>
+            </label>
+        </div>
+    );
 }
