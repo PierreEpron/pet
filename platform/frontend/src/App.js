d073070dbe95d9e9b0932b8cf00792ee29cd544a
MickaSimplon <mickael.simlon@gmail.com>
2021-06-25 13:38:09 +0200

    back 1st verion darkmode

--- platform/frontend/src/App.js
+++ platform/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignIn from "./components/SignIn";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import HomePage from './pages/HomePage'
@@ -11,30 +11,34 @@ import deepPurple from '@material-ui/core/colors/deepPurple';
 
 
 function App() {
-    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)');
     const theme = React.useMemo(
     () =>
       createMuiTheme({
         palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
+          type: prefersDarkMode ? 'light' : 'dark',
             primary: deepPurple,
             secondary: deepPurple,
         },
       }),
     [prefersDarkMode],
     );
+
+
+
     return (
         <div className="App">
             <ThemeProvider theme={theme}>
             <CssBaseline/>
+
             <Router>
                 <Switch>
+
                     <Route path="/" exact component={SignIn}/>
                     <Route path="/HomePage" exact component={HomePage}/>
                     <Route path="/document/:id" exact component={Docpage}/>
                     <Route path="/StatPage" exact component={StatPage}/>
-                    {/*<Route path="/pagestest" exact component={() => <PageTest />} />*/}
+
                 </Switch>
             </Router>
             </ThemeProvider>
