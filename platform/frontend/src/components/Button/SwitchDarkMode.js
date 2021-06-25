42a745a79821962db16307f05096d99073b8a589
MickaSimplon <mickael.simlon@gmail.com>
2021-06-25 09:54:57 +0200

    test switch dark mode

--- platform/frontend/src/components/Button/SwitchDarkMode.js
+++ platform/frontend/src/components/Button/SwitchDarkMode.js
@@ -1,15 +1,26 @@
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import React, { useState } from "react";
 import Switch from "@material-ui/core/Switch";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+
 
 export default function DarkMode() {
   const [darkState, setDarkState] = useState(false);
   const palletType = darkState ? "dark" : "light";
-  const darkTheme = createMuiTheme({
-    palette: {
-      type: palletType,
-    }
-  });
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const darkTheme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+            primary: deepPurple,
+            secondary: deepPurple,
+        },
+      }),
+    [prefersDarkMode],
+    );
   const handleThemeChange = () => {
     setDarkState(!darkState);
   };
