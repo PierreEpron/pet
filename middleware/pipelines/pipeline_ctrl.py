b9a87393d8443fd5d5f976e17cceac9f1edc2dad
PierreEpron <pierre.epron@gmail.com>
2021-06-25 14:39:38 +0200

    add PipelineCtrl

---
+++ middleware/pipelines/pipeline_ctrl.py
@@ -0,0 +1,16 @@
+class PipelineCtrl:
+    def __init__(self, version) -> None:
+        self.name = self.__class__.__name__
+        self.version = version
+    
+    def __call__(self, text) -> dict:
+        return {}
+
+    def get_fullname(self) -> str:
+        return f'{self.name}_{self.version}'
+
+    def add_feature(self, features : dict, newFeatures : dict) -> None:        
+        fname, flist = newFeatures
+        if fname not in features:
+            features[fname] = {}
+        features[fname][self.get_fullname()] = flist
