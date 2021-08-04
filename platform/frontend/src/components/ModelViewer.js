d095cc6ccfcf081941144dd9038b6aaf5443dce6
dupre761 <60468515+dupre761@users.noreply.github.com>
2021-08-04 14:41:40 +0200

    add unstylized ModelViewer

---
+++ platform/frontend/src/components/ModelViewer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Switch from '@material-ui/core/Switch';  
+import FormControlLabel from '@material-ui/core/FormControlLabel'; 
+
+export default function ModelViewer(props) {
+    // const classes = useStyles();
+    const {modelData} = props;
+    const [modelState, setModelState] = React.useState(modelData.state);
+    
+    console.log(modelData)
+    return (
+        <Paper>
+            <Typography>
+                {modelData.name}
+            </Typography>
+            <FormControlLabel
+                control={
+                    <Switch
+                        checked={modelState}
+                        onChange={(e) => setModelState(e.target.checked)}
+                        name="modelState"
+                        color="primary"
+                         />
+                }
+                label="Active"
+            />
+            <Typography>
+                {modelData.desc}
+            </Typography>
+        </Paper>
+        )
+}
