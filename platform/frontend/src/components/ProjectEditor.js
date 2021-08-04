f2c6754f00d1e838fc3b2719789e6ff989f9782e
dupre761 <60468515+dupre761@users.noreply.github.com>
2021-08-04 15:44:05 +0200

    fix deleteButton style

--- platform/frontend/src/components/ProjectEditor.js
+++ platform/frontend/src/components/ProjectEditor.js
@@ -6,6 +6,8 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ModelViewer from './ModelViewer';
 import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+
 
 
 const models = [
@@ -13,19 +15,30 @@ const models = [
     {name:'Dim', state: true, desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
     
 ]
+
+const useStyles = makeStyles((theme) => ({
+    deleteButton: {
+        display: 'flex',
+        position: 'absolute',
+        right: '32px',
+
+}}));
+
 export default function ProjectEditor(props) {
-    // const classes = useStyles();
+    const classes = useStyles();
     // const [loading, setLoading] = React.useState(false);
     const {projectData} = props
     console.log(projectData)
     return (
         <Paper>
-            <Typography>
-                {projectData.name} ({projectData.nbDocument} documents)
-            </Typography>
-            <IconButton color="primary" aria-label="Delete Project">
-                <DeleteIcon ></DeleteIcon>
-            </IconButton>
+            <FormGroup row>
+                <Typography>
+                    {projectData.name} ({projectData.nbDocument} documents)
+                </Typography>
+                <IconButton className ={classes.deleteButton} color="primary" aria-label="Delete Project">
+                    <DeleteIcon ></DeleteIcon>
+                </IconButton>
+            </FormGroup>
             {models.map((element, i) => <ModelViewer modelData = {models[i]}></ModelViewer>)}
             <Button variant="contained" color="secondary">Cancel</Button>
             <Button variant="contained" color="primary">Save</Button>
