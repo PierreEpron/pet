171f57357da8d1f164fc298329ee75e12f74fb96
dupre761 <60468515+dupre761@users.noreply.github.com>
2021-08-04 14:42:05 +0200

    add unstylized ProjectEditor

---
+++ platform/frontend/src/components/ProjectEditor.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ModelViewer from './ModelViewer';
+import Button from '@material-ui/core/Button';
+
+
+const models = [
+    {name:'Pet', state: true, desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+    {name:'Dim', state: true, desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+    
+]
+export default function ProjectEditor(props) {
+    // const classes = useStyles();
+    // const [loading, setLoading] = React.useState(false);
+    const {projectData} = props
+    console.log(projectData)
+    return (
+        <Paper>
+            <Typography>
+                {projectData.name} ({projectData.nbDocument} documents)
+            </Typography>
+            <IconButton color="primary" aria-label="Delete Project">
+                <DeleteIcon ></DeleteIcon>
+            </IconButton>
+            {models.map((element, i) => <ModelViewer modelData = {models[i]}></ModelViewer>)}
+            <Button variant="contained" color="secondary">Cancel</Button>
+            <Button variant="contained" color="primary">Save</Button>
+
+
+        </Paper>
+        )
+}
