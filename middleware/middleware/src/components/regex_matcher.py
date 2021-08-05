7b7bf037d8dad1cd0708ce63eeea0a3a3b55e864
PierreEpron <pierre.epron@gmail.com>
2021-08-05 12:39:52 +0200

    fix regex_matcher

--- middleware/middleware/src/components/regex_matcher.py
+++ middleware/middleware/src/components/regex_matcher.py
@@ -3,37 +3,37 @@ from spacy.tokens import Doc
 import re, json
 from src.helpers import get_patterns
 
-@Language.factory("regex_matcher", default_config={"patterns": []})
-def create_regex_matcher_component(nlp: Language, name: str, patterns: list):
+@Language.factory("regex_matcher", default_config={"patterns": {}})
+def create_regex_matcher_component(nlp: Language, name: str, patterns: dict):
     return RegexMatcherComponent(nlp, patterns)
 
 class RegexMatcherComponent:
-    def __init__(self, nlp: Language, patterns: list):
-        self.patterns = self.load_patterns(get_patterns(patterns))
+    def __init__(self, nlp: Language, patterns: dict):
+        print(patterns)
+        self.patterns = {k:self.load_patterns(get_patterns(v)) for k, v in patterns.items()}
         if not Doc.has_extension("regex_matchs"):
-            Doc.set_extension("regex_matchs", default=[])
+            Doc.set_extension("regex_matchs", default={})
 
     def __call__(self, doc: Doc) -> Doc:
-        
+        for k, v in self.patterns.items():
+            for label, pattern in v.items():
+                doc._.regex_matchs[k] = [{'start':match.start(0), 'end':match.end(0), 'label':label} for match in pattern.finditer(doc.text)]
         return doc
 
     def to_disk(self, path, exclude=tuple()):
         data_path = path / "patterns.json"
-        
         if not path.is_dir():
             path.mkdir()
-
-        data_path.write_text(json.dumps({k:v for k, v in get_patterns(list(self.patterns.keys())).items()}), encoding="utf-8")
+        data_path.write_text(json.dumps(self.patterns), encoding="utf-8")
 
     def from_disk(self, path, exclude=tuple()):        
         data_path = path / "patterns.json"
-
-        self.patterns = json.loads(data_path.read_text())
+        self.patterns = json.loads(data_path.read_text(), encoding="utf-8")
 
     def load_patterns(self, patterns):
         return {k:re.compile(v, re.I) for k, v in patterns.items()}
 
 def extract_matchs(doc):
-    pass
-    # return 'sections', [{'start':section.start_char, 'end':section.end_char, 'label':section.label_} for section in doc._.sections]      
+    return [(k, v) for k, v in doc._.regex_matchs.items()]
+    
 
--- middleware/middleware/src/pipelines/alpha.py
+++ middleware/middleware/src/pipelines/alpha.py
@@ -3,22 +3,27 @@ import spacy
 from thinc.api import Config
 from .pipeline_ctrl import PipelineCtrl
 from src.components import section_splitter
+from src.components import regex_matcher
 from src.helpers import extract_sents
 
 class Alpha(PipelineCtrl):
     def __init__(self) -> None:
         super().__init__(version="0.1", force_update=False)
         self.sections_patterns = ['intro', 'ctx', 'tech', 'obs', 'ccl']
+        self.regex_patterns = {'identity':['gender', 'age'], 'medical':['suvmax', 'medobj_size']}
         self.build_model()
 
     def __call__(self, text, features) -> None:
         doc = self.nlp(text)
         self.add_feature(features, section_splitter.extract_sections(doc))
         self.add_feature(features, extract_sents(doc))
-
+        for feature in regex_matcher.extract_matchs(doc):
+            self.add_feature(features, feature)
+            
     def build_model(self) -> None:
         self.nlp = spacy.load("fr_dep_news_trf")
         self.nlp.add_pipe("sections_splitter", config={"patterns": self.sections_patterns})
+        self.nlp.add_pipe("regex_matcher", config={"patterns": self.regex_patterns}),
 
     def load_model(self, path) -> None:
         path = Path(path) if isinstance(path, str) else path
