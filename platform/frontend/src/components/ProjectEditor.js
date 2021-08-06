84db30c3e82e75ab006462924ba06c81d27efec8
dupre761 <60468515+dupre761@users.noreply.github.com>
2021-08-06 10:36:08 +0200

    connect model info to frontend

--- platform/frontend/src/components/ProjectEditor.js
+++ platform/frontend/src/components/ProjectEditor.js
@@ -8,14 +8,9 @@ import ModelViewer from './ModelViewer';
 import Button from '@material-ui/core/Button';
 import FormGroup from '@material-ui/core/FormGroup';
 import Grid from '@material-ui/core/Grid';
+import {getContents, postContent} from "../services/content.service";
+import Progress from "../components/CircularProgress/CircularProgress";
 
-
-
-const models = [
-    {name:'Pet', state: true, desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
-    {name:'Dim', state: true, desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
-    
-]
 
 const useStyles = makeStyles((theme) => ({
     deleteButton: {
@@ -37,9 +32,34 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ProjectEditor(props) {
     const classes = useStyles();
-    // const [loading, setLoading] = React.useState(false);
+
+    const [modelInfo, setModelInfo] =  React.useState (null)
+    const [isLoading, setIsLoading] =  React.useState (true)
+
     const {projectData} = props
-    console.log(projectData)
+
+    const updateModelInfo = React.useCallback((data) => {
+        const newData = data.map((element) => {
+            const name = element.name
+            const desc = element.desc
+            const state = projectData.active_models.includes(name)
+            return {name, desc, state}
+        })
+        setModelInfo(newData)
+    }, [setModelInfo]);
+
+    React.useEffect(() => {
+        if (modelInfo)
+            setIsLoading(false)
+        else {
+            setIsLoading(true)
+            getContents('/models-info/', {}, updateModelInfo)
+        }
+    }, [modelInfo, updateModelInfo, setIsLoading]);
+
+    if (isLoading)
+        return (<div><Progress/></div>)
+
     return (
         <Paper className= {classes.paper}>
             <FormGroup row>
@@ -50,7 +70,7 @@ export default function ProjectEditor(props) {
                     <DeleteIcon ></DeleteIcon>
                 </IconButton>
             </FormGroup>
-            {models.map((element, i) => <ModelViewer modelData = {models[i]}></ModelViewer>)}
+            {modelInfo.map((element, i) => <ModelViewer modelData = {modelInfo[i]}></ModelViewer>)}
             <Grid className= {classes.controlButtons} container direction="row" justifyContent="flex-end" spacing= {2}> 
                 <Grid item >
                     <Button variant="contained" color="secondary">Cancel</Button>
