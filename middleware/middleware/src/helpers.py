082767e0b2b36e477074887a80efa9fbcb458fa3
PierreEpron <pierre.epron@gmail.com>
2021-08-08 14:56:54 +0200

    add write_jsonl in middleware helper

--- middleware/middleware/src/helpers.py
+++ middleware/middleware/src/helpers.py
@@ -33,3 +33,6 @@ def read_jsonl(path, encoding='utf-8'):
     path = Path(path) if isinstance(path, str) else path
     return [json.loads(line) for line in path.read_text(encoding=encoding).strip().split('\n')]
 
+def write_jsonl(path, data, encoding='utf-8'):
+    path = Path(path) if isinstance(path, str) else path
+    path.write_text('\n'.join([json.dumps(item) for item in data]), encoding=encoding)
