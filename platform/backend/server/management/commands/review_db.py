e5e8cfea6905108e04b192ca70e30dc972da7970
PierreEpron <pierre.epron@gmail.com>
2021-08-21 17:24:42 +0200

    add review_db command

--- platform/backend/server/management/commands/review_db.py
+++ platform/backend/server/management/commands/review_db.py
@@ -3,16 +3,52 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'List tables in db'
+    help = '''Shorcuts for review database'''
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('--poll_seconds', type=float, default=3)
-    #     parser.add_argument('--max_retries', type=int, default=60)
+    def add_arguments(self, parser):
+        parser.add_argument('--table', type=str, default='')
+        parser.add_argument('--columns', default=False, action='store_true')
+        parser.add_argument('--join', type=str, default='')
+        parser.add_argument('--select', type=str, default='*')
+        parser.add_argument('--where', type=str, default='')
 
     def handle(self, *args, **options):
-        tables = connection.introspection.table_names()
-        print(tables)
+        table = options.get('table')
+        columns = options.get('columns')
+        
+        if table == '':
+            tables = connection.introspection.table_names()
+            print(tables)
+        elif columns:
+            cursor = connection.cursor()
+            cursor.execute(
+                f'''
+                    SELECT *
+                    FROM information_schema.columns
+                    WHERE table_name = '{table}'
+                '''
+            )
+            print('\n'.join([str(line) for line in cursor.fetchall()]))
+        else:
+            join = options.get('join').split('.')
+            select = options.get('select')
+            where = options.get('where')
+ 
+            join_raw = ''
+            if len(join) == 2:
+                join_raw = f'LEFT JOIN {join[0]} ON {table}.{join[1]} = {join[0]}.id'
 
-        # seen_models = connection.introspection.installed_models(tables)
-        # print([f.name for f in list(seen_models)[4]._meta.get_fields()])
+            where_raw = where if where == '' else f'WHERE {where}'
+
+            cursor = connection.cursor()
+            cursor.execute(
+                f'''
+                    SELECT {select}
+                    FROM {table}
+                    {join_raw}
+                    {where_raw}
+                '''
+            )
+            print('\n'.join([str(line) for line in cursor.fetchall()]))
+
         
