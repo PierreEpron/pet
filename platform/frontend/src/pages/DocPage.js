9434f5a62aa212c13b79cd063bfda42f7b17c6d8
PierreEpron <pierre.epron@gmail.com>
2021-08-23 20:49:02 +0200

    make horizontal spacing of docpage smaller

--- platform/frontend/src/pages/DocPage.js
+++ platform/frontend/src/pages/DocPage.js
@@ -14,8 +14,8 @@ const useStyles = makeStyles((theme) => ({
     container: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4),
-        paddingLeft:theme.spacing(8),
-        paddingRight: theme.spacing(8),
+        paddingLeft:theme.spacing(4),
+        paddingRight: theme.spacing(4),
     },
     panel : {
         height:'78vh',
