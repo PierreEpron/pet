fd611689744748e7be0b332f85d26161539be037
PierreEpron <pierre.epron@gmail.com>
2021-08-27 11:02:28 +0200

    add apply task

---
+++ middleware/middleware/tasks.py
@@ -0,0 +1,34 @@
+from os import name
+from src.pipelines.alpha import Alpha
+from src.pipelines.dim import Dim
+from src.stats import word_list_freq
+from models import MODELS
+import functools
+import json
+import time
+
+def apply_task(data):
+    data = json.loads(data)
+    text = data['text']
+    features = data['features']
+    active_models = data['active_models']
+    if isinstance(features, list) and len(features) > 0:
+        model_to_skips = set(functools.reduce(lambda a, b: a + b, [item['name'] for item in features]))
+    else:
+        model_to_skips = set()
+        features = []
+    for model_class in MODELS:
+        if model_class.get_fullname() in active_models and (model_class.force_update == True or model_class.get_fullname() not in model_to_skips):
+            model = model_class()
+            et = time.process_time()
+            model(text, features)
+            features.append(
+                {'name':'time_to_compute', 
+                    'sources': [{
+                        'name': model_class.get_fullname(),
+                        'type': 'model',
+                        'items': [{'label':'ttc', 'value':str(time.process_time()-et)}]
+                }]})                        
+    word_frequencies = word_list_freq(text)
+    
+    return data['id'], {'features':features,'word_frequencies':word_frequencies}
