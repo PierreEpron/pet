814072d6fa8c6d1dbafca4cb665a4c80c2b672c8
PierreEpron <pierre.epron@gmail.com>
2021-08-28 21:16:58 +0200

    adapt dim for futur loading

--- middleware/middleware/src/pipelines/dim.py
+++ middleware/middleware/src/pipelines/dim.py
@@ -12,7 +12,7 @@ class Dim(PipelineCtrl):
     desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     force_update=False
 
-    def __init__(self) -> None:
+    def __init__(self, path) -> None:
         super().__init__()
         self.build_model()
 
